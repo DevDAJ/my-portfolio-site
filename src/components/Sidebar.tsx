d484973b993024bc4a3e6894980cc567d456ca82
Danial Aiman Jamil <58195654+KawakazeRin@users.noreply.github.com>
2023-01-13 03:33:42 +0800

    fix: more fixes

--- src/components/Sidebar.tsx
+++
@@ -1,7 +0,0 @@
-export default function Sidebar() {
-	return (
-		<div className='sticky' style={{ writingMode: 'vertical-rl' }}>
-			<p>{'About me > Skills > Awards & Certifications'}</p>
-		</div>
-	);
-}
