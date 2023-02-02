613603487ae75138a49e2e6d417705b62c6b5368
Danial Aiman Jamil <58195654+KawakazeRin@users.noreply.github.com>
2023-02-02 23:33:26 +0800

    partytown

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { Icon } from '@iconify/react';
 import './index.css';
+import { Head } from './components/Head';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<BrowserRouter>
+			<Head />
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>
@@ -27,9 +29,9 @@ function Socials() {
 		{ name: 'whatsapp', url: 'https://wa.me/+601110165333/' },
 	];
 	return (
-		<div className='[display:none;] md:flex flex-col-reverse gap-3 max-w-[3rem] justify-start px-2 self-end'>
+		<div className='reverse gap-3 max-w-[3rem] justify-start px-2 self-end'>
 			{socials.map((social) => (
-				<a href={social['url']}>
+				<a key={social['name']} href={social['url']}>
 					<Icon width={'2rem'} icon={`mdi:${social['name']}`} />
 				</a>
 			))}
